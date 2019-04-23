d2f85bab3eaf3d3de1b138b17f32267af9f97e84
Étienne Lacasse <thimbledice@gmail.com>
2019-04-23 08:31:04 -0400

    Liste des personnes impliquées

--- Project Simon/TestBeauceApp/src/screens/about/about.js
+++ Project Simon/TestBeauceApp/src/screens/about/about.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View, ScrollView, SectionList, Text, StyleSheet } from 'react-native';
+import Colors from '../../res/colors';
+import Fonts from '../../res/fonts';
+import firebase from 'react-native-firebase';
 
 export default class About extends Component {
     
@@ -9,16 +12,74 @@ export default class About extends Component {
 
   constructor(props){
     super(props);
+    this.ref = firebase.firestore().collection('Collaborateur');
+    this.unsubscribe = null;
+    this.state = {
+      collaborators: [],
+      loading: true
+    };
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const collaborators = {};
+    querySnapshot.forEach((doc) => {
+      if(typeof collaborators[doc.data().Title] === "undefined"){
+        collaborators[doc.data().Title] = [];
+      }
+      collaborators[doc.data().Title].push(doc.data().Name);
+    });
+    const sectionListCollaborators = [];
+    Object.keys(collaborators).map((title, index) =>{
+      sectionListCollaborators.push({title: title, data: collaborators[title]});
+    });
+    this.setState({
+      collaborator: sectionListCollaborators,
+      loading: false,
+    });
   }
 
   componentDidMount(){
     console.log(this.props.navigation.state);
+    this.unsubscribe = this.ref.orderBy('Name', "ASC").onSnapshot(this.onCollectionUpdate);
   }
+
   render() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world About!</Text>
-      </View>
-    );
+    if(this.state.loading){
+      return null;
+    } else {
+      return (
+        <View style={styles.container}>
+          <SectionList
+            sections={this.state.collaborator}
+            renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+            renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+            keyExtractor={(item, index) => index}
+          />
+        </View>
+      );
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: Colors.black,
+    backgroundColor: Colors.primary,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  item: {
+    padding: 10,
+    fontSize: 16,
+    height: 44,
+  },
+})
