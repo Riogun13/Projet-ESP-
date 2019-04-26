12cc496e80d817460edc2b523ba0186c72cecf67
SimonRoy <simoroy94@gmail.com>
2019-04-26 13:02:54 -0400

    ajout du splash screen

--- Project Simon/TestBeauceApp/android/app/src/main/java/com/testbeauceapp/MainApplication.java
+++ Project Simon/TestBeauceApp/android/app/src/main/java/com/testbeauceapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.testbeauceapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -34,6 +36,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new SplashScreenReactPackage(),
           new LocationServicesDialogBoxPackage(),
           new RNFirebasePackage(),
           new RNFirebaseFirestorePackage(),
