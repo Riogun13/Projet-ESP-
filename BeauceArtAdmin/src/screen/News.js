e92fcc65573b5a68053f3a71f250979f49486bfd
Pierre-Luc Bégin <pluc113@hotmail.com>
2019-05-03 13:44:54 -0400

    modification de certaine parie d'affichage

--- BeauceArtAdmin/src/screen/News.js
+++ BeauceArtAdmin/src/screen/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Colors from '../res/colors';
 
 export default class News extends Component {
   constructor(props){
@@ -17,9 +18,9 @@ export default class News extends Component {
 
     onCollectionUpdate = (querySnapshot) => {
 
-      const sculptures = {};
+      const nouvelles = {};
       querySnapshot.docs.forEach(doc => {
-        nouvelles[doc.data().Thematic.Year][doc._ref._documentPath._parts[1]] = doc.data();
+        nouvelles[doc._ref._documentPath._parts[1]] = doc.data();
       });
       this.setState({
         nouvelles: nouvelles,
@@ -40,34 +41,28 @@ export default class News extends Component {
             <ScrollView style={styles.container}>
               {
                 Object.keys(this.state.nouvelles).map((year, index) =>(
-                    <CollapsiblePanel
-                      title={"Édition "+ year}
-                      key={year}
-                      style={styles.collapsiblePanel}
-                    >
                       {
                         Object.keys(this.state.sculptures[year]).map((sculptueId, index) =>(
                           <TouchableOpacity
                             style={styles.button}
-                            key={sculptueId}
+                            key={nouvelleId}
                             onPress={() => this.props.navigation.push(
-                              'SculptureForm', 
+                              'NewsForm', 
                               {
-                                sculpture: this.state.sculptures[year][sculptueId],
-                                sculptureId: sculptueId
+                                nouvelle: this.state.nouvelles[sculptueId],
+                                nouvelleId: nouvelleId
                               })}
                           >
-                            <Text style={styles.buttonText}>{this.state.sculptures[year][sculptueId].Name}</Text>
+                            <Text style={styles.buttonText}>{this.state.nouvelles[sculptueId].Name}</Text>
                           </TouchableOpacity>
                         ))
                       }
-                    </CollapsiblePanel>
                 ))
               }
             </ScrollView>
             <TouchableOpacity
                     style={styles.mapButton}
-                      onPress={()=> this.props.navigation.push('SculptureForm')}
+                      onPress={()=> this.props.navigation.push('NewsForm')}
                   >
                     <Ionicons name={"ios-add"}  size={40} color={Colors.text} />
                   </TouchableOpacity>
--- BeauceArtAdmin/src/screen/Sculptures.js
+++ BeauceArtAdmin/src/screen/Sculptures.js
@@ -23,7 +23,7 @@ export default class SculptureList extends Component {
   
   onCollectionUpdate = (querySnapshot) => {
 
-    const nouvelles = {};
+    const sculptures = {};
     querySnapshot.docs.forEach(doc => {
       if(typeof sculptures[doc.data().Thematic.Year] === "undefined"){ //lookup if there is already a year
         sculptures[doc.data().Thematic.Year] = {};
